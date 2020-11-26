08a46456b81ef3aaace505da1a1789130dbf4f7a
GitHub Action <action@github.com>
2020-11-26 07:50:37 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/fsancheztemprano/typescriptfolding/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.fsancheztemprano.typescriptfolding.services
+
+import com.github.fsancheztemprano.typescriptfolding.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/fsancheztemprano/typescriptfolding/services/MyProjectService.kt
+++ src/main/kotlin/com/github/fsancheztemprano/typescriptfolding/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.fsancheztemprano.typescriptfolding.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.fsancheztemprano.typescriptfolding.MyBundle
 
 class MyProjectService(project: Project) {
 
